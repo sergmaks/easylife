98bc59a033798390c564da5816180faf982d9fe5
sergmaks <maximov.sv90@mail.ru>
2016-09-17 22:53:24 +0400

    Full Content in Template (Use jquery-noconflict + small fixes)

--- js/template.js
+++ js/template.js
@@ -1,83 +1,81 @@
-/****   Custom JQuery  *****/
-        
-        var footerShowed = false;
-        
-        // Select right size of background images in slider
-        function chooseSize(){
-            var folder; // folder for choosing images
-            var slidesCount = $('.back').size(); // number of carousel slides
-            
-            if ( $(window).width() < 1200 )
-                folder = 'middle';
-            else if ( $(window).width() < 1600 )
-                folder = 'large';
-            else if ( $(window).width() < 2000 )
-                folder = 'x-large';
-            else folder = 'xx-large';
-            
-            //alert(folder);
-        }
-        
-        // Set the carousel height to the user screen height without header
-        function setCarouselHeight(){
-            $('.back').css({
-                height: ($(document).height() - 
-                         $('#top-bar').outerHeight() - 
-                         $('#nav-bar').outerHeight()) + "px"
-                           })
-        }
-        
-        // Show info footer by scrolling down to the end of the page
-        function showFooter(){
-            $('#footer').slideDown();
-            $('html, body').animate( { scrollTop: $(window).height() }, 2000 );
-            $('#button-up').show();
-            footerShowed = true;
-        }
-        
-        function hideFooter(){
-            $('#footer').slideUp(1000);
-            $('#button-down').show();
-            footerShowed = false;
-        }
-        
-        // Enable Bootstrap tooltips and Set initial carousel height
-        $(document).ready(
-            function(){
-                //$("#attention").remove(); // Delete JS warning
-                $('[data-toggle="tooltip"]').tooltip(); 
-                setCarouselHeight();
-            });
-        
-        
-        // Window events: 
-        // Set carousel height when the window is resized
+/****   Custom jQuery  *****/
+var footerShowed = false;
+  
+// Select right size of background images in slider
+function chooseSize(){
+    var folder; // folder for choosing images
+    var slidesCount = jQuery('.back').size(); // number of carousel slides
+      
+    if ( jQuery(window).width() < 1200 )
+  folder = 'middle';
+    else if ( jQuery(window).width() < 1600 )
+  folder = 'large';
+    else if ( jQuery(window).width() < 2000 )
+  folder = 'x-large';
+    else folder = 'xx-large';
+ //alert(folder);
+  }
+  
+  // Set the carousel height to the user screen height without header
+  function setCarouselHeight(){
+      jQuery('.back').css({
+    height: (jQuery(document).height() - 
+       jQuery('#top-bar').outerHeight() - 
+       jQuery('#nav-bar').outerHeight()) + "px"
+         })
+  }
+  
+  // Show info footer by scrolling down to the end of the page
+  function showFooter(){
+      jQuery('#footer').slideDown();
+      jQuery('html, body').animate( { scrollTop: jQuery(window).height() }, 2000 );
+      jQuery('#button-up').show();
+      footerShowed = true;
+  }
+  
+  function hideFooter(){
+      jQuery('#footer').slideUp(1000);
+      jQuery('#button-down').show();
+      footerShowed = false;
+  }
+  
+  // Enable Bootstrap tooltips and Set initial carousel height
+  jQuery(document).ready(
+      function(){
+    //jQuery("#attention").remove(); // Delete JS warning
+    jQuery('[data-toggle="tooltip"]').tooltip();
+    setCarouselHeight();
+      });
+  
+  
+  // Window events: 
+  // Set carousel height when the window is resized
 
-        $(window).resize(
-            function(){
-                if ( $(window).height() > 600 ) {
-                    $('.back').css({
-                        height: ( $(window).height() - 
-                                  $('#top-bar').outerHeight() - 
-                                  $('#nav-bar').outerHeight()) + "px"
-                                  })
-                    }
-                    });
-        
-        // Hide footer when the window is scrolled to the top
-        $(window).scroll(function(){
-            if ( ($(window).scrollTop() == 0) 
-                && (footerShowed == true) )  
-                                            hideFooter();
-         });
-        
-        // Buttons Down and Up events:
-        $('#button-down').click(function (){
-            $(this).hide();
-            showFooter();
+  jQuery(window).resize(
+      function(){
+    if ( jQuery(window).height() > 600 ) {
+        jQuery('.back').css({
+      height: ( jQuery(window).height() - 
+          jQuery('#top-bar').outerHeight() - 
+          jQuery('#nav-bar').outerHeight()) + "px"
+          })
+        }
         });
-        
-         $('#button-up').click(function(){
-             $(this).hide();
-             hideFooter();
-         });
+  
+  // Hide footer when the window is scrolled to the top
+  jQuery(window).scroll(function(){
+      if ( (jQuery(window).scrollTop() == 0) 
+    && (footerShowed == true) )  
+        hideFooter();
+   });
+  
+  // Buttons Down and Up events:
+  jQuery('#button-down').click(function (){
+      jQuery(this).hide();
+      showFooter();
+  });
+  
+   jQuery('#button-up').click(function(){
+       jQuery(this).hide();
+       hideFooter();
+   });
