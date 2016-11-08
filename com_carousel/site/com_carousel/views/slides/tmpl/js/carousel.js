1aa20606b10834519f965774b2fae69ea8efe903
325aud <325aud@google.com>
2016-11-08 13:50:21 +0300

    Working with custom script

--- com_carousel/site/com_carousel/views/slides/tmpl/js/carousel.js
+++ com_carousel/site/com_carousel/views/slides/tmpl/js/carousel.js
@@ -1,14 +1,22 @@
+
+// Set the carousel height when the document is loaded
 jQuery(document).ready(
         function(){
-            jQuery('.back').css({
+                var prevHeight = 0;
+                var elements = document.getElementsByTagName('body')[0].getElementsByTagName('div');
+                for (var i=0; i < elements.length; i++) {
+                    if (elements[i].id == 'carousel') break;
+                    prevHeight += elements[i].height;
+                }
+                alert (prevHeight);
+                jQuery('.back').css({
                 height: (jQuery(document).height() - 
                 jQuery('#top-bar').outerHeight() - 
                 jQuery('#nav-bar').outerHeight()) + "px"
             });
       });
 
-// Window events: 
-// Set slides height when the window is resized
+// Set the carousel height when the window is resized
 
   jQuery(window).resize(
           function(){
