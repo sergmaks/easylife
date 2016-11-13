5ad59ea842967c3b341e8cf1681846526e878a58
sergmaks <maximov.sv90@mail.ru>
2016-11-13 20:42:34 +0300

    Final Installation File

--- com_carousel/admin/com_carousel/sql/install.mysql.utf8.sql
+++ com_carousel/admin/com_carousel/sql/install.mysql.utf8.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS `#__carousel` (
     `id` INT(11) NOT NULL AUTO_INCREMENT , 
     `ordering` INT(11) NOT NULL , 
     `published` TINYINT(4) NOT NULL , 
-    `caption` VARCHAR(50) NOT NULL , 
+    `caption` VARCHAR(1024) NOT NULL , 
     `image` VARCHAR(1024) NOT NULL , 
     `icon` VARCHAR(25) NOT NULL , 
     `date` DATETIME NOT NULL , 
