57c3610d194f50d8eb3ef363385fd1de948c5b1b
sergmaks <sergmaks@github.com>
2016-11-18 14:53:58 +0300

    работа с CSS

---
+++ modules/mod_joomly_callback/sql/install.mysql.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `#__joomly_callback` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+ `phone` varchar(100) NOT NULL,
+  `call_time` varchar(100) NOT NULL,
+ `created_at` varchar(100) NOT NULL,
+
+ PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
