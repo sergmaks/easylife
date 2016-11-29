cfa7ced19ebc0ac38ce5f21f17125a85e6cea70a
sergmaks <sergmaks@github.com>
2016-11-29 13:40:33 +0300

    Mobile Menu Final

--- templates/easylife/js/template.js
+++ templates/easylife/js/template.js
@@ -1,76 +1,47 @@
 /****   Custom jQuery  *****/
 var footerShowed = false;
 
-var showMobileDropdown = function(){
-    if ( jQuery(this).children('ul').is(':visible') ) {
-        jQuery(this).children('ul').slideUp('normal');
-        
-        jQuery('#nav-bar').animate(
-            {height: jQuery('#nav-bar').outerHeight() - jQuery(this).children('ul').outerHeight() + "px"},
-            'normal');         
-    }
-    else {
-        jQuery(this).children('ul').slideDown('normal');
-        
-        jQuery('#nav-bar').animate(
-             {height: jQuery('#nav-bar').outerHeight() + jQuery(this).children('ul').outerHeight() + "px"},
-            'normal');
-    }
+var showMobileDropdown = function() {
+    jQuery(this).children('ul').slideToggle('normal');     
+}
 
-}
-  
-function setMobileMenuHeight(){
-    if ( jQuery('#nav-bar').is(':visible') ) {
-         jQuery('#nav-bar').outerHeight(
-                    jQuery('#menu-container').outerHeight()
-                 +  jQuery('#socials-container').outerHeight()
-                 +  26 );
-     }
-}
-  
-function checkMobile(){      
-        
-    if ( jQuery(document).width() > 767 ) { // if desktop or tablet
-            
+// Checking sreen width and adding some parameters
+function checkMobile() {          
+    if ( jQuery(document).width() > 767 ) {  // if desktop or tablet            
         jQuery('#nav-bar').outerHeight(50);
         jQuery('#nav-bar').show();
         jQuery('#nav-bar li > ul').show();
         jQuery('[data-toggle="tooltip"]').tooltip('enable');
         jQuery('#nav-bar li').unbind('click', showMobileDropdown);
-    }
-        
-    else { // if mobile
-            
-        setMobileMenuHeight();
-        jQuery('#nav-bar').hide();// hide menu block
-        jQuery('#nav-bar li > ul').hide();// hide dropdown menu
-        jQuery('[data-toggle="tooltip"]').tooltip('disable'); // disable bootstrap tooltips
+    } else {  // if mobile
+        jQuery('#nav-bar').css({height:"auto"});    
+        jQuery('#nav-bar').hide();  // hide menu block
+        jQuery('#nav-bar li > ul').hide();  // hide dropdown menu
+        jQuery('[data-toggle="tooltip"]').tooltip('disable');  // disable bootstrap tooltips
         jQuery('#nav-bar li').unbind('click', showMobileDropdown);
         jQuery('#nav-bar li').bind('click', showMobileDropdown);
     }
 }
     
 // Show info footer by scrolling down to the end of the page
-function showFooter(){
+function showFooter() {
     jQuery('#footer').slideDown();
-    jQuery('html, body').animate( { scrollTop: jQuery(window).height() }, 2000 );
+    jQuery('html, body').animate( {scrollTop: jQuery(window).height()}, 2000 );
     jQuery('#button-up').show();
     footerShowed = true;
 }
   
-function hideFooter(){
+function hideFooter() {
     jQuery('#footer').slideUp(1000);
     jQuery('#button-down').show();
     footerShowed = false;
 }
   
 jQuery(document).ready(
-    function(){
-        jQuery('.main-nav').attr({"data-toggle":"tooltip", "data-placement":"bottom"});  // Enable Bootstrap tooltips     
-       
+    function() {
+        jQuery('.main-nav').attr({"data-toggle":"tooltip", "data-placement":"bottom"});  // Enable Bootstrap tooltips            
         jQuery('#menu-trigger').click(function(){
-            jQuery('#nav-bar li > ul').hide();// hide dropdown menu
-            setMobileMenuHeight();
+            jQuery('#nav-bar li > ul').hide();  // hide dropdown menu
             jQuery('#nav-bar').slideToggle('normal');
             
         });
@@ -79,25 +50,26 @@ jQuery(document).ready(
 });
    
 // Checking mobile display when window is resized
-jQuery(window).resize(function(){
+jQuery(window).resize(function() {
     checkMobile();
 }); 
    
 // Hide footer when the window is scrolled to the top
-jQuery(window).scroll(function(){
+jQuery(window).scroll(function() {
     if ( (jQuery(window).scrollTop() == 0) 
-        && (footerShowed == true) )  
+      && (footerShowed == true) ) {  
             hideFooter();
+    }
 });
   
 // Buttons Down and Up events:
-jQuery('#button-down').click(function (){
+jQuery('#button-down').click(function () {
     jQuery(this).hide();
     showFooter();
 });
   
-jQuery('#button-up').click(function(){
-    jQuery(this).hide();
+jQuery('#button-up').click(function() {
+    jQuery(this).hide(); 
     hideFooter();
 });
 
